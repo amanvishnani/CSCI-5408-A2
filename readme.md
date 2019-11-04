0045008e037ac26e8c47f0a9773c836d8b1737f3
Aman Vishnani <aman.vishnani@dal.ca>
2019-11-04 18:21:01 -0400

    Now using bigrams

---
+++ readme.md
@@ -0,0 +1,1 @@
+# Please install python dependencies listed in the `requirements.txt`
--- spark_task.py
+++ spark_task.py
@@ -1,21 +1,41 @@
-keywords = ["education", "canada", "university", "dalhousie", "expensive", "faculty", "graduate", "good-school", "bad-school", "computer-science"]
-bigram_keywords = [("good school", "good-school"), ("bad school", "bad-school"),
-                   ("computer science", "computer-science")]
+from pyspark import SparkConf, SparkContext
+
+try:
+    sc
+except Exception as e:
+    sc = None
+    conf = SparkConf().setAppName("Assignment2")
+    sc = SparkContext(conf=conf)
+
+keywords = ["education", "canada", "university", "dalhousie", "expensive", "faculty", "graduate"]
+bigram_keywords = ["good school", "bad school", "computer science"]
 
 
-def replaceWords(sentence):
-    for m, n in bigram_keywords:
-        sentence = sentence.replace(m, n)
-    return sentence
+def bigrams(wordList):
+    new_words = []
+    for i in range(len(wordList) - 1):
+        new_words.append((wordList[i], wordList[i + 1]))
+    return new_words
 
 
-# Read file
 fileRDD = sc.textFile('file:///home/ubuntu/output.txt')
 
+sentences = fileRDD.map(lambda sentence: sentence.lower())
+sentences_word = sentences.map(lambda line: line.split(" ")).cache()
 
-sentences = fileRDD.map(lambda sentence: sentence.lower()).map(replaceWords)
-words = sentences.flatMap(lambda line: line.lower().split(" "))
-
+# Pipeline 1
+words = sentences_word.flatMap(lambda x: x)
 filtered_word = words.filter(lambda word: word in keywords)
-filtered_word_count = filtered_word.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
-filtered_word_count.collect()
+
+# Pipeline 2
+bigram_words = sentences_word.flatMap(lambda s: bigrams(s))
+bigram_words = bigram_words.map(lambda word_tuple: " ".join([word_tuple[0], word_tuple[1]]))
+bigram_words = bigram_words.filter(lambda word: word in bigram_keywords)
+
+# Concatenate
+final_words = filtered_word.union(bigram_words)
+final_word_count = final_words.map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+
+final_word_count.cache()
+final_word_count.saveAsTextFile("/root/spark_output")
+final_word_count.collect()
